947a1bda49b1a23d9eb2de3c707dbb34779a9168
Brandon Grady <nbhsbg53@gmail.com>
2022-11-22 21:39:56 -0500

    footer

--- develop/script.js
+++ develop/script.js
@@ -1,11 +1,13 @@
 //Users enters a super hero name
-
-
+var searchBarEl = document.querySelector("#searchBar");
+var searchBtnEl = document.querySelector("#searchBtn");
+var header = $('#header')
 // Harcoded to wolverine, but this should be an event listener to whatever the user clicks
 let userSuperHero = 'wolverine'
 
 
-const container = document.querySelector('.container');
+const container = document.querySelector('.containerHero');
+const containerHeroInfo = document.querySelector('.containerHeroInfo');
 
 //call the marvel characters API
 const marvel = {
@@ -16,18 +18,25 @@ const marvel = {
         fetch(apiCharactersMarvel)
         .then(res => res.json())
         .then((json) => {
+            console.log(json)
             for (const hero of json.data.results) {
                 let urlHero = hero.urls[0].url;
                 let heroId = hero.id
                 contentHTML += `
-                <div class="hero-container">
+                <figure class="media-left ">
+                <div class="image is-128x128">
                     <a href="${urlHero}" target="_blank">
-                        <img src="${hero.thumbnail.path}.${hero.thumbnail.extension}" alt="${hero.name}" class="img-thumbnail">
+                    <img src="${hero.thumbnail.path}.${hero.thumbnail.extension}" alt="${hero.name}" class="img-thumbnail">
                     </a>
-                    <h2 class="Title">${hero.name}</h2>
-                    <h3>${heroId}</h3>
-                    <p class="description">${hero.description}</p>
-                <div>
+                </div>
+                </figure>
+                <div class="media-content">
+                <div class="content has-text-black">
+                    <h1 class="content has-text-white is-underlined is-bolder">${hero.name}</h1>
+                    <h4>Id no.${heroId}</h4>
+                    <p class="content has-text-white has-text-weight-semibold is-italic">${hero.description}</p>
+                </div>
+                </div>
                 `
             }
             container.innerHTML = contentHTML;
@@ -36,3 +45,72 @@ const marvel = {
 };
 
 marvel.render();
+
+const rawg = {
+    render: () => {
+        const apiRawg = 'https://api.rawg.io/api/games?key=d5bbf27705f64c2bbfd9e4ce1a23cc6d&page_size=5&search_precise=true&search=' + userSuperHero
+        let contentHTML = ''
+        console.log(apiRawg)
+        fetch(apiRawg)
+        .then(res => res.json())
+        .then((json) => {
+            console.log(json)
+            for (const game of json.results) {
+                let nameGame = game.name;
+                let platformGame = game.platforms[0].platform.name
+                let releaseGame = game.released
+                let urlGame = game.background_image
+                let scoreGame = game.score
+                contentHTML += `
+                    <article class="column containerCards notification is-black m-3">
+                        <div class="content">
+                        <figure class="image is-3by2">
+                        <img src="${urlGame}" alt="${nameGame}">
+                        </figure>
+                        </div>         
+                        <div class="content">
+                        <p class="content is-size-4 mb-6">${nameGame}</p>
+                        <p class="content is-size-6">Platform: ${platformGame} <br> Release Date: ${releaseGame}</p>
+                        Overall Score = ${scoreGame}
+                        </div>
+                    </article>
+                `
+            }
+            containerHeroInfo.innerHTML = contentHTML
+        })
+    }
+}
+rawg.render();
+
+//Function for Hero Search
+function handleSearch(storedBtnValue) {
+    console.log("handleSearch");
+    var userSearch = searchBarEl.value;
+    console.log(userSearch);
+    userSuperHero = userSearch || storedBtnValue;
+    marvel.render();
+    rawg.render();
+    localStorage.setItem('storedHero',userSearch)
+    renderButtons()
+}
+searchBtnEl.addEventListener("click", handleSearch);
+
+header.on('click',function (event){
+    if (!event.target.matches('.recentSearch')) return
+    let storedBtnValue = event.target.innerHTML
+    handleSearch(storedBtnValue)
+})
+// let storedHero = JSON.parse(localStorage.getItem("storedHero")) || []
+
+function renderButtons () {
+    let storedHero = localStorage.getItem('storedHero')
+      let buttonHistory = $('<button>').html(storedHero).attr('id',storedHero).addClass('recentSearch')
+      header.append(buttonHistory)
+
+  }
+
+// function recentSearch (event) {
+//     if (!event.target.matches('.recentSearch')) return
+//     let storedBtnValue = event.target.innerHTML
+//     handleSearch(storedBtnValue)
+// } 
